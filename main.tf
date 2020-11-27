e5e6aff48b330eabb45632b575f289890227cfd5
Zelimir Fedoran <zelimir.fedoran@gmail.com>
2020-11-27 16:46:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ data "aws_region" "default" {
 }
 
 module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.17.0"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=master"
   enabled    = var.enabled
   namespace  = var.namespace
   name       = var.name
@@ -150,7 +150,7 @@ data "aws_iam_policy_document" "codebuild" {
 }
 
 module "codebuild" {
-  source                      = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=tags/0.17.0"
+  source                      = "git::https://github.com/cloudposse/terraform-aws-codebuild.git?ref=master"
   enabled                     = var.enabled
   namespace                   = var.namespace
   name                        = var.name
